2fe4614f138ef67adcdd328799cf76e4b4f358bb
Y．K <yk@KY.local>
2019-02-18 20:38:52 +0900

    change to testnet version

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ app.use(express.static('web'));
 const bitcoin = require('bitcoinjs-lib');
 const bip32 = require('bip32');
 const base58check = require('base58check');
-const NETWORK = bitcoin.networks.mainnet;
+const NETWORK = bitcoin.networks.testnet;
 const pubKey = 'xprv9s21ZrQH143K25QhxbucbDDuQ4naNntJRi4KUfWT7xo4EKsHt2QJDu7KXp1A3u7Bi1j8ph3EGsZ9Xvz9dGuVrtHHs7pXeTzjuxBrCmmhgC6';
 
 var index = 0;
-var parent = bip32.fromBase58(pubKey, NETWORK);
+//var parent = bip32.fromBase58(pubKey, NETWORK);
 
 function getAddress(node){
     return bitcoin.payments.p2pkh({pubkey:node.publicKey, network:NETWORK}).address;
@@ -18,10 +18,10 @@ function getAddress(node){
 
 //app.get('/', (req, res) => res.send('Hello'));
 app.get('/api/v1/invoice', (req, res) => {
-    var node = parent.derivePath("m/0'/" + String(index));
+    //var node = parent.derivePath("m/0'/" + String(index));
     index++;
-    //const address = "n14trMejKPL8HMBj2EhQkctsDfhMXHMtkJ";
-    const address = getAddress(node);
+    const address = "n14trMejKPL8HMBj2EhQkctsDfhMXHMtkJ";
+    //const address = getAddress(node);
     const ret_val = {invoice: 'bitcoin:' + address + "?amount=" + req.query.amount,
                     btc_address: address};
     res.json(ret_val);
