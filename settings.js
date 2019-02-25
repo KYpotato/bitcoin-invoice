0a8d3d247cc171c703f99c99dca3d74cbf1da5d4
KYpoteto <s0methingfun313@gmail.com>
2019-02-25 11:12:24 +0900

    mod settings

--- settings.js
+++ settings.js
@@ -1,3 +1,3 @@
-exports.port = 3000;
-exports.pubkey = 'tpubD8eQVK4Kdxg3gHrF62jGP7dKVCoYiEB8dFSpuTawkL5YxTus5j5pf83vaKnii4bc6v2NVEy81P2gYrJczYne3QNNwMTS53p5uzDyHvnw2jm';
-exports.test_address = "n14trMejKPL8HMBj2EhQkctsDfhMXHMtkJ";
+exports.port = process.env.PORT;
+exports.pubkey = process.env.PUBKEY;
+exports.test_address = process.env.TEST_ADDRESS;
