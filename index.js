f76d700eeadf3523785f56d69631cbc022ad5584
KYpoteto <s0methingfun313@gmail.com>
2019-02-25 11:01:26 +0900

    add setting.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 
 const express = require('express');
 const app = express();
+const settings = require('./settings');
 app.use(express.static('web'));
 
 const bitcoin = require('bitcoinjs-lib');
 const bip32 = require('bip32');
 const base58check = require('base58check');
 const NETWORK = bitcoin.networks.testnet;
-const pubKey = 'tpubD8eQVK4Kdxg3gHrF62jGP7dKVCoYiEB8dFSpuTawkL5YxTus5j5pf83vaKnii4bc6v2NVEy81P2gYrJczYne3QNNwMTS53p5uzDyHvnw2jm';
+const pubKey = settings.pubkey;
+const test_address = settings.test_address;
 
 var index = 0;
 var parent = bip32.fromBase58(pubKey, NETWORK);
@@ -19,11 +21,11 @@ function getAddress(node){
 app.get('/api/v1/invoice', (req, res) => {
     var node = parent.derivePath(String(index));
     index++;
-    //const address = "n14trMejKPL8HMBj2EhQkctsDfhMXHMtkJ";
+    //const address = test_address;
     const address = getAddress(node);
     const ret_val = {invoice: 'bitcoin:' + address + "?amount=" + req.query.amount,
                     btc_address: address};
     res.json(ret_val);
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+app.listen(settings.port, () => console.log('Listening on port ' + settings.port));
