cc8e431b6bce0cc652127d187fb5e5862f7c18cc
KYpoteto <s0methingfun313@gmail.com>
2019-02-25 17:28:04 +0900

    add process to read index from textfile and write index to text file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const settings = require('./settings');
 app.use(express.static('web'));
+var fs = require('fs');
 
 const bitcoin = require('bitcoinjs-lib');
 const bip32 = require('bip32');
@@ -11,7 +12,18 @@ const NETWORK = bitcoin.networks.testnet;
 const pubKey = settings.pubkey;
 const test_address = settings.test_address;
 
-var index = 0;
+console.log(settings.index_file);
+
+var index;
+try {
+    fs.statSync(settings.index_file)
+    var file = fs.readFileSync(settings.index_file, 'utf-8');
+    index = file;
+    console.log("file content:" + file);
+} catch(err){
+    index = 0;
+}
+
 var parent = bip32.fromBase58(pubKey, NETWORK);
 
 function getAddress(node){
@@ -21,6 +33,11 @@ function getAddress(node){
 app.get('/api/v1/invoice', (req, res) => {
     var node = parent.derivePath(String(index));
     index++;
+    fs.writeFile(settings.index_file, index, function(err) {
+        if(err){
+            console.log(err);
+        }
+    });
     //const address = test_address;
     const address = getAddress(node);
     const ret_val = {invoice: 'bitcoin:' + address + "?amount=" + req.query.amount,
