c6dcab7aa62e1aee0593526290db679cdeb3ec3e
KYpoteto <s0methingfun313@gmail.com>
2019-03-04 16:48:46 +0900

    revert delete settings.js

---
+++ settings.js
@@ -0,0 +1,9 @@
+exports.port = process.env.PORT;
+exports.pubkey = process.env.PUBKEY;
+exports.test_address = process.env.TEST_ADDRESS;
+exports.mongodb_uri = process.env.MONGODB_URI;
+exports.indexdb = process.env.INDEXDB;
+
+//exports.port = 3000;
+//exports.pubkey = 'tpubD8eQVK4Kdxg3gHrF62jGP7dKVCoYiEB8dFSpuTawkL5YxTus5j5pf83vaKnii4bc6v2NVEy81P2gYrJczYne3QNNwMTS53p5uzDyHvnw2jm';
+//exports.test_address = "n14trMejKPL8HMBj2EhQkctsDfhMXHMtkJ";
